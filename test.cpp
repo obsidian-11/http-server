f128a33fa9e1bc12673a42cd156d6f210d70b79b
Anees <syedaneesibrahom@gmail.com>
2025-06-02 23:19:54 +0000

    inital commit

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	// Creating a HTTP Server
+	// Step 1: Create a socket and listen for new connections.
+	// Step 2: Accept incoming client connections.
+	// Step 3: Receive messages, process them, and send some responses to the clients.
+	// Step 4: Close the connection.
+}
